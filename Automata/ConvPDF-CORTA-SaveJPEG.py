86c0c653abeb584520d47a5563f1571adc63edb3
Yahir <yahire419@gmail.com>
2024-08-11 15:53:08 -0500

    Commit inicial

---
+++ Automata/ConvPDF-CORTA-SaveJPEG.py
@@ -0,0 +1,29 @@
+import os
+from PyPDF2 import PdfWriter, PdfFileReader #Esta libreria importa metodos para leer PDF
+from pdf2image import convert_from_path     #Esta libreria importa metodos para convertir PDF a JPG  
+from icecream import ic #Esta libreria hace que se vea ordenado al entregar datos
+from math import ceil, trunc #Esta libreria ayuda a dividir y cortar las paginas de los pdf
+from tkinter import Tk # Esta libreria ayuda con los cuadros de texto
+from tkinter.filedialog import askopenfilename, asksaveasfilename #Esta libreria ayuda a seleccionar el archivo PDF
+
+#Esta linea es la ruta para los archivos que convierten PDF
+poppler_path= r"C:\Users\yahir\Desktop\FusionXY Technologies\AUTOMATAS\App\poppler-0.68.0\bin"
+
+#Esta linea es la funcion que selecciona el archivo pdf a convertir
+pdf_path = askopenfilename(title="Selecciona el archivo PDF de entrada", filetypes=[("Archivos PDF", "*.pdf")])
+
+#Esta linea convierte el archivo pdf a jpg
+pages= convert_from_path(pdf_path= pdf_path, poppler_path=poppler_path)
+
+#Esta linea selecciona donde se van a guardar las imagenes
+saving_folder= r"C:\Users\yahir\Desktop\FusionXY Technologies\AUTOMATAS\App\pages"
+
+#Aqui se cortan y guargan las paginas 
+c=1
+
+for page in pages:
+    img_name= f"img-{c}.jpeg"
+    page.save(os.path.join(saving_folder,img_name),"JPEG")
+    c+=1
+
+ic ("Conversión completa")
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi primer repositorio 
+
+Aqui encontraras diferentes scripts que te pueden a ayudar a utomatizar tareas simples ya sea, desde convertir un archivo pdf a jpeg hasta lograr que tengas una red de minibots que hagan tareas repetitivas que te ayuden a monetizar contenido online.
